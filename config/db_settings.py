b0bb9b1c5ef2882b02c49b0050646ffdb2e2a592
Saksham Gupta <idreturned1@gmail.com>
2020-11-01 16:28:53 +0530

    updated login and onboarding api to return onboarding status

--- config/db_settings.py
+++ config/db_settings.py
@@ -1,9 +1,9 @@
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'vivs_theta',
-        'USER': 'vivs_theta',
-        'PASSWORD': 'vivs_theta',
+        'NAME': 'theta',
+        'USER': 'theta',
+        'PASSWORD': 'theta',
         'HOST': '127.0.0.1',
         'PORT': '5432'
     }
